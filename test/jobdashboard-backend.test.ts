9332118b0d2656de07b4e30c8f9b85554e17ee3d
vishnukumarkvs <kvsvishnukumar@gmail.com>
2023-06-10 10:49:23 +0530

    Initial commit

---
+++ test/jobdashboard-backend.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as JobdashboardBackend from '../lib/jobdashboard-backend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/jobdashboard-backend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new JobdashboardBackend.JobdashboardBackendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});

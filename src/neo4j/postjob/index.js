d61b65876fb126cb433d94951e60e43b2ae1f6c5
vishnukumarkvs <kvsvishnukumar@gmail.com>
2023-06-23 16:49:20 +0530

    added userid, company. Also refactored query

--- src/neo4j/postjob/index.js
+++ src/neo4j/postjob/index.js
@@ -7,36 +7,41 @@ exports.handler = async function (event) {
   console.log("event data:", data);
 
   const createJobQuery = `
-      WITH $restData AS jobData, $skills AS skillsData, $experienceUnit AS expUnitValue, $salaryUnit AS salUnitValue, $locations AS locationsArray
-      CREATE (job:Job {
-          jobTitle: jobData.jobTitle,
-          baseExp: jobData.baseExp,
-          highExp: jobData.highExp,
-          baseSalary: jobData.baseSalary,
-          highSalary: jobData.highSalary,
-          date: datetime(jobData.date),
-          description: jobData.description,
-          created_at: datetime(),
-          updated_at: datetime()
-      })
-      WITH job, skillsData, expUnitValue, salUnitValue, locationsArray
-      FOREACH (skillValue IN skillsData |
-          MERGE (skill:Skill {value: skillValue})
-          MERGE (job)-[:REQUIRES_SKILL]->(skill)
-      )
-      WITH job, expUnitValue, salUnitValue, locationsArray
-      MERGE (expUnit:Unit {value: expUnitValue, type: "experience"})
-      MERGE (job)-[:HAS_UNIT]->(expUnit)
-      WITH job, salUnitValue, locationsArray
-      MERGE (salUnit:Unit {value: salUnitValue, type: "salary"})
-      MERGE (job)-[:HAS_UNIT]->(salUnit)
-      WITH job, locationsArray
-      UNWIND locationsArray as locationEntry
-      MERGE (city:City {name: locationEntry.city})
-      MERGE (country:Country {name: locationEntry.country})
-      MERGE (city)-[:IN_COUNTRY]->(country)
-      MERGE (job)-[:IN_CITY]->(city);
-  `;
+  CREATE (job:Job {
+    jobTitle: $restData.jobTitle,
+    baseExp: $restData.baseExp,
+    highExp: $restData.highExp,
+    baseSalary: $restData.baseSalary,
+    highSalary: $restData.highSalary,
+    date: datetime($restData.date),
+    description: $restData.description,
+    created_at: datetime(),
+    updated_at: datetime()
+  })
+  WITH job
+  FOREACH (skillValue IN $skills |
+    MERGE (skill:Skill {value: skillValue})
+    MERGE (job)-[:REQUIRES_SKILL]->(skill)
+  )
+  WITH job
+  MERGE (expUnit:Unit {value: $experienceUnit, type: "experience"})
+  MERGE (job)-[:HAS_UNIT]->(expUnit)
+  WITH job
+  MERGE (salUnit:Unit {value: $salaryUnit, type: "salary"})
+  MERGE (job)-[:HAS_UNIT]->(salUnit)
+  WITH job
+  UNWIND $locations as locationEntry
+  MERGE (city:City {name: locationEntry.city})
+  MERGE (country:Country {name: locationEntry.country})
+  MERGE (city)-[:IN_COUNTRY]->(country)
+  MERGE (job)-[:IN_CITY]->(city)
+  MERGE (com:Company {name: $company})
+  MERGE (job)-[:AT_COMPANY]->(com)
+  WITH job
+  MATCH (user:User {id: $userid})
+  CREATE (user)-[:POSTED_JOB]->(job)
+  RETURN job;
+`;
 
   const session = driver.session({ database: "neo4j" });
 
@@ -48,6 +53,8 @@ exports.handler = async function (event) {
         experienceUnit: data.experienceUnit,
         salaryUnit: data.salaryUnit,
         locations: data.locations,
+        company: data.company,
+        userid: data.userid,
       })
     );
     console.log(`Write result:`, writeResult);

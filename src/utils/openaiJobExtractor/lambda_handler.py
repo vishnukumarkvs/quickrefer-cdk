2eca6bbdb51943a3fd816cbd5a14d7c5bda9e73c
vishnukumarkvs <kvsvishnukumar@gmail.com>
2023-07-22 00:02:42 +0530

    openai extracter working

--- src/utils/openaiJobExtractor/lambda_handler.py
+++ src/utils/openaiJobExtractor/lambda_handler.py
@@ -52,13 +52,49 @@ def lambda_handler(event, context):
         functions=function_descriptions,
         function_call="auto",  # specify the function call
     )
+ 
+    # print("completion")
+    # print(completion)
 
-    print(completion)
+
     
-    output = completion.choices[0].message['content']
-    print(output)
+    output = completion.choices[0].message
+
+
+    # print("printing output")
+    # print(output)
+
+    job_info_string = output['function_call']['arguments']
+
+    # Parse the JSON string into a dictionary
+    job_info = json.loads(job_info_string)
+
+    # Now you can access the fields
+    job_poster = job_info['job_poster']
+    job_title = job_info['job_title']
+    job_description = job_info['job_description']
+    job_location = job_info['job_location']
+    technical_skills = job_info['technical_skills']
+
+    # Print them out
+    # print('Job Poster:', job_poster)
+    # print('Job Title:', job_title)
+    # print('Job Description:', job_description)
+    # print('Job Location:', job_location)
+    # print('Technical Skills:', technical_skills)
+
+    job_data = {
+    'Job Poster': job_poster,
+    'Job Title': job_title,
+    'Job Description': job_description,
+    'Job Location': job_location,
+    'Technical Skills': technical_skills
+    }
+
+    # Convert the Python dictionary into a JSON string
+    job_data_json = json.dumps(job_data)
     
     return {
         'statusCode': 200,
-        'body': output
+        'job_data': job_data_json
     }

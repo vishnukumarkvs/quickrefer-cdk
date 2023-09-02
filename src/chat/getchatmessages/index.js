d135dc6215c4891a41ba752ed277191b51f83a77
vishnukumarkvs <kvsvishnukumar@gmail.com>
2023-09-02 17:06:18 +0530

    fixed cors error

--- src/chat/getchatmessages/index.js
+++ src/chat/getchatmessages/index.js
@@ -26,12 +26,22 @@ exports.handler = async (event) => {
     console.log("Queried data", data.Items);
     return {
       statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*", // You might want to restrict this in production
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Content-Type": "application/json",
+      },
       body: JSON.stringify(data.Items),
     };
   } catch (error) {
     console.error("Unable to query. Error:", JSON.stringify(error, null, 2));
     return {
       statusCode: 500,
+      headers: {
+        "Access-Control-Allow-Origin": "*", // You might want to restrict this in production
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Content-Type": "application/json",
+      },
       body: JSON.stringify({ message: "Internal Server Error" }),
     };
   }
